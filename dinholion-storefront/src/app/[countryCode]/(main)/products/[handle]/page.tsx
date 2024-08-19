47d5b8ee2c44d52b8fb023aa6175b401d19d3246
Lukas Laudrain <lukas.ldrn@gmail.com>
2024-08-19 21:57:06 +0200

    fix: disabled the products static params generation

--- dinholion-storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ dinholion-storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -15,34 +15,34 @@ type Props = {
   params: { countryCode: string; handle: string }
 }
 
-export async function generateStaticParams() {
-  const countryCodes = await listRegions().then((regions) =>
-    regions?.map((r) => r.countries.map((c) => c.iso_2)).flat()
-  )
-
-  if (!countryCodes) {
-    return null
-  }
-
-  const products = await Promise.all(
-    countryCodes.map((countryCode) => {
-      return getProductsList({ countryCode })
-    })
-  ).then((responses) =>
-    responses.map(({ response }) => response.products).flat()
-  )
-
-  const staticParams = countryCodes
-    ?.map((countryCode) =>
-      products.map((product) => ({
-        countryCode,
-        handle: product.handle,
-      }))
-    )
-    .flat()
-
-  return staticParams
-}
+// export async function generateStaticParams() {
+//   const countryCodes = await listRegions().then((regions) =>
+//     regions?.map((r) => r.countries.map((c) => c.iso_2)).flat()
+//   )
+//
+//   if (!countryCodes) {
+//     return null
+//   }
+//
+//   const products = await Promise.all(
+//     countryCodes.map((countryCode) => {
+//       return getProductsList({ countryCode })
+//     })
+//   ).then((responses) =>
+//     responses.map(({ response }) => response.products).flat()
+//   )
+//
+//   const staticParams = countryCodes
+//     ?.map((countryCode) =>
+//       products.map((product) => ({
+//         countryCode,
+//         handle: product.handle,
+//       }))
+//     )
+//     .flat()
+//
+//   return staticParams
+// }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { handle } = params
