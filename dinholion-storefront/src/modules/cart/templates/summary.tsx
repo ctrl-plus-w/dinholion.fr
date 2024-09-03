10a139cdb3559b5cf3fca307e40e513152fff07c
SHOPBX1 <159017705+SHOPBX1@users.noreply.github.com>
2024-09-03 07:50:54 +0200

    Update summary.tsx

--- dinholion-storefront/src/modules/cart/templates/summary.tsx
+++ dinholion-storefront/src/modules/cart/templates/summary.tsx
@@ -16,13 +16,13 @@ const Summary = ({ cart }: SummaryProps) => {
   return (
     <div className="flex flex-col gap-y-4">
       <Heading level="h2" className="text-[2rem] leading-[2.75rem]">
-        Summary
+        Résumé
       </Heading>
       <DiscountCode cart={cart} />
       <Divider />
       <CartTotals data={cart} />
       <LocalizedClientLink href={"/checkout?step=" + cart.checkout_step} data-testid="checkout-button">
-        <Button className="w-full h-10">Go to checkout</Button>
+        <Button className="w-full h-10">Voir le panier</Button>
       </LocalizedClientLink>
     </div>
   )
